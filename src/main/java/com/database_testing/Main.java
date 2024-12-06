2dabea39b6154d51a14ee9aeda5e63704b62b6bd
moearkarlwin <moearkarlwin@gmail.com>
2024-12-06 13:44:20 +0630

    Development First Commit

--- src/main/java/com/database_testing/Main.java
+++ src/main/java/com/database_testing/Main.java
@@ -1,10 +1,28 @@
 package com.database_testing;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+    private Connection conn;
+
+    public void get_Connection() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/world_population?" +
+                    "user=root&password=");
+            System.out.println("Successful Connection");
+        }
+        catch (SQLException sqle) {
+            System.out.println("SQLException: " + sqle.getMessage());
+            System.out.println("SQLState: " + sqle.getSQLState());
+            System.out.println("VendorError: " + sqle.getErrorCode());
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        Main m = new Main();
+        m.get_Connection();
+
     }
 }
